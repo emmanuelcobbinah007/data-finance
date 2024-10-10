3bf589133213b82286c8e4f12b8db49396740245
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-10-10 20:57:06 +0000

    worked on first part of the Hero Banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+    //State to handle Navbar
     const [nav, setNav] = useState(false);
 
     const navHandler = () => {
         setNav(!nav)
     }
 
+
+    // Actual Navbar
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
         <ul className='hidden md:flex hidden'>
             <li className='p-4'>Home</li>
