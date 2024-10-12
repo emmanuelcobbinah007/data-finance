d03365333d0a4baf29433b89c6d10135d27376a1
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-10-12 12:20:19 +0000

    Start of the work day

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,14 @@ const Hero = () => {
   return (
     <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
         <p className='text-[#00df9a] font-bold p-2 uppercase'>Growing With Data Analytics</p>
-        <h1 className='font-bold md:text-7xl sm:text-6xl text-4xl md:py-6'>Grow with data.</h1>
+        <h1 className='font-bold md:text-6xl sm:text-5xl text-3xl md:py-4'>Grow with data.</h1>
         <div>
-            <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, flexible financing for</p>
+            <p className='md:text-4xl sm:text-3xl text-xl font-bold py-4'>Fast, flexible financing for</p>
         </div>
+        <p className='md:text-xl text-large font-bold text-gray-500'>
+            Monitor your data analytics to increase revenue fot BTB, BTC & SASS platforms
+        </p>
+        <button className='bg-[#00df9a] w-[150px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black'>Get Started</button>
     </div>
   )
 }
