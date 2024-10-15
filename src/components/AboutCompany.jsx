6028dc73415032a18b08d1f87fdefa1fdcb6019e
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-10-15 21:15:29 +0000

    Did some work on the sidebar and finished up the first section the company page

--- src/components/AboutCompany.jsx
+++ src/components/AboutCompany.jsx
@@ -4,15 +4,26 @@ import team from '../assets/team.jpg'
 const AboutCompany = () => {
   return (
     <div>
-        <div className='h-screen w-full max-w-[1240px] mx-auto grid grid-cols-5 py-6'>
-            <div className='col-span-3 p-6'>
+        <div className='hidden lg:h-full lg:w-full lg:max-w-[1240px] lg:mx-auto lg:my-1 lg:grid lg:grid-cols-2 lg:py-3 lg:px-5 lg:pb-6'>
+            <div className='col-span-1 p-6 text-center'>
             <h1 className='font-bold md:text-4xl sm:text-1xl text-2xl md:py-4'>Who are we?</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ea voluptates laboriosam nam tempore numquam autem corrupti doloribus facere earum consequuntur dicta minus rerum, officiis nulla veritatis quae doloremque delectus. Ex repellat laboriosam necessitatibus labore nemo placeat reiciendis obcaecati. Expedita molestias facilis molestiae asperiores, eveniet recusandae soluta.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, ipsam laboriosam quis cum cupiditate explicabo repudiandae earum excepturi laudantium unde sed illo deserunt delectus repellendus culpa eum dicta? Similique quam voluptatum aliquid quaerat? Optio tenetur recusandae excepturi, labore et, voluptas animi rerum, at quaerat dignissimos amet! Corrupti nulla ea nobis mollitia dicta asperiores eligendi accusamus minus deleniti eius.</p>
             </div>
-            <div>
-            <img src={team} alt="/"  className='bg-white w-[300px] mx-auto px'/>
+            <div className='px-12 py-20'>
+            <img src={team} alt="/"  className='w-[500px] bg-white mx-auto rounded-md hover:scale-105 duration-300'/> 
             </div>
+        </div>
+        <div className='lg:hidden w-full max-w-[1240px] mx-auto my-3 grid py-3 px-5'>
+            <div className='px-12 py-3'>
+            <img src={team} alt="/"  className='w-[500px] bg-white mx-auto rounded-md'/> 
+            </div>
+            <div className='col-span-1 p-6 text-center'>
+            <h1 className='font-bold md:text-4xl sm:text-1xl text-2xl md:py-4'>Who are we?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ea voluptates laboriosam nam tempore numquam autem corrupti doloribus facere earum consequuntur dicta minus rerum, officiis nulla veritatis quae doloremque delectus. Ex repellat laboriosam necessitatibus labore nemo placeat reiciendis obcaecati. Expedita molestias facilis molestiae asperiores, eveniet recusandae soluta.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, ipsam laboriosam quis cum cupiditate explicabo repudiandae earum excepturi laudantium unde sed illo deserunt delectus repellendus culpa eum dicta? Similique quam voluptatum aliquid quaerat? Optio tenetur recusandae excepturi, labore et, voluptas animi rerum, at quaerat dignissimos amet! Corrupti nulla ea nobis mollitia dicta asperiores eligendi accusamus minus deleniti eius.</p>
+            </div>
+            
         </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,8 +28,8 @@ const Navbar = () => {
         <div className={`fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#000300] ease-in-out duration-500 ${nav ? 'left-0' : 'left-[-100%]'} md:hidden`}>
         <h1 className='m-4 w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
             <ul className='uppercase p-4'>
-            <li className='p-4 border-b border-b-gray-600 cursor-pointer hover:text-[#00df9a]'>Home</li>
-            <li className='p-4 border-b border-b-gray-600 cursor-pointer hover:text-[#00df9a]'>Company</li>
+            <Link to='/'><li className='p-4 border-b border-b-gray-600 cursor-pointer hover:text-[#00df9a]'>Home</li></Link>
+            <Link to='/company'><li className='p-4 border-b border-b-gray-600 cursor-pointer hover:text-[#00df9a]'>Company</li></Link>
             <li className='p-4 border-b border-b-gray-600 cursor-pointer hover:text-[#00df9a]'>Resources</li>
             <li className='p-4 border-b border-b-gray-600 cursor-pointer hover:text-[#00df9a]'>About</li>
             <li className='p-4 cursor-pointer hover:text-[#00df9a]'>Contact</li>
