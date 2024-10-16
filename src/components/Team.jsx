0a420a241971bff2554d7f6fac9dc8c90333373f
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-10-16 20:23:25 +0000

    partly solved the problem at teams

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -32,10 +32,10 @@ const Team = () => {
   return (
     <div className='text-black bg-white'>
       <div className=' max-w-[1240px] m-auto'>
-        <div className='max-w-[1000px] mx-auto lg:text-left text-center'> 
+        <div className='relative z-10 bg-white max-w-[1000px] mx-auto lg:text-left text-center'> 
           <h1 className='text-[#00df9a] font-bold md:text-6xl sm:text-5xl text-3xl md:py-6'>The Team</h1>
         </div>
-        <div className='z-10 grid grid-cols-2 py-6 bg-white'>
+        <div className='relative z-10 grid grid-cols-2 py-6 bg-white'>
             <div className='flex justify-center'>
             <img className='rounded-xl lg:w-[300px] md:w-[250px] shadow-xl mr-[-25%]' src={Picture1} alt="/" />
             </div>
@@ -44,7 +44,7 @@ const Team = () => {
               <p className='text-[#00df9a] font-bold text-center'>Founder, Managing Director</p>
             </div>
           </div>
-          <div className={`z-5 ease-in-out duration-300 ${arrow ? 'mt-[-500px]' : 'mt-2 my-8 flex flex-col justify-center'}`}>
+          <div className={`relative z-5 ease-in-out duration-300 ${arrow ? 'mt-[-660px]' : 'mt-2 my-8 flex flex-col justify-center'}`}>
           <div className='grid grid-cols-4 gap-20 mx-auto'>
             {people.map(person => (
               <div className='text-center' key={person.id}>
@@ -54,7 +54,7 @@ const Team = () => {
               </div>))} 
           </div>
           </div>
-          <div className='max-w-[1240px]'  onClick={turn}>
+          <div className='max-w-[1240px] pt-[60px]' onClick={turn}>
             {arrow ? <FaAngleDown className='text-4xl mx-auto'/> : <FaAngleUp className='text-4xl mx-auto'/> }
           
           </div>
