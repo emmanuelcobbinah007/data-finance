35b7bc7e2384da333ff6750d8b5d559a08d1aaa4
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-10-18 22:20:17 +0000

    Final Commit Work Done

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Laptop from '../assets/laptop.jpg'
+import { Link } from 'react-router-dom';
 
 const Analytics = () => {
   return (
@@ -10,7 +11,7 @@ const Analytics = () => {
                 <p className='uppercase text-[#00df9a] font-bold md:text-left text-center'>Data Analytics Dashboard</p>
                 <h1 className='md:text-3xl text-2xl font-bold py-1 md:text-left text-center'>Manage Data Analytics Centrally</h1>
                 <p className='md:text-left text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam iusto quaerat architecto ad omnis consectetur delectus porro illum vel, voluptatibus sint. Error eius quo, adipisci placeat iusto delectus quisquam qui.</p>
-                <button className='text-[#00df9a] bg-black w-[180px] md:mx-0 m-auto rounded-md py-2 my-4'>Get Started</button>
+                <button className='text-[#00df9a] bg-black w-[180px] md:mx-0 m-auto rounded-md py-2 my-4'><Link to='/sign-up'>Get Started</Link></button>
             </div>
         </div>
     </div>
